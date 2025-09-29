910ca298141520b9d4460d4568571ef4acd55eb9
Daniel <danibasketball2000@gmail.com>
2025-09-29 09:53:00 +0200

    Initial commit

---
+++ nnm/src/main/java/com/nnm/nnm/NnmApplication.java
@@ -0,0 +1,13 @@
+package com.nnm.nnm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NnmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NnmApplication.class, args);
+	}
+
+}
---
+++ nnm/src/test/java/com/nnm/nnm/NnmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nnm.nnm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NnmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
