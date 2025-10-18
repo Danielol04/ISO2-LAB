4de6d3f071a68e23f16a7edf1d9d50b08a52e04b
elterror6 <danibasketball2000@gmail.com>
2025-10-18 11:55:49 +0200

    Adición de la entidad DAO del inquilino.

--- nnm/src/main/java/com/nnm/nnm/negocio/dominio/entidades/Inquilino.java
+++ nnm/src/main/java/com/nnm/nnm/negocio/dominio/entidades/Inquilino.java
@@ -1,12 +1,18 @@
 package com.nnm.nnm.negocio.dominio.entidades;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "inquilino")
-public class Inquilino extends Usuario {
 
+public class Inquilino extends Usuario {
+	@Id
+	private Long id;
+	
+	@OneToOne
+	@JoinColumn(name="username")
+	private Usuario usuario;
+	
     public Inquilino(String login, String pass, String nombre, String apellidos, String direccion) {
         super(login, pass, nombre, apellidos, direccion);
     }
