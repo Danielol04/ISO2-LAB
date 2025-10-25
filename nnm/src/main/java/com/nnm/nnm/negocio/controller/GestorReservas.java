5a907f723ed484f1ee1395f173f19df38c8d5d3c
Csauul <saulcorbelle@gmail.com>
2025-10-25 18:59:04 +0200

    Correccion GestorReservas

--- nnm/src/main/java/com/nnm/nnm/negocio/controller/GestorReservas.java
+++ nnm/src/main/java/com/nnm/nnm/negocio/controller/GestorReservas.java
@@ -1,50 +1,41 @@
 package com.nnm.nnm.negocio.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.stereotype.Service;
 
 import com.nnm.nnm.negocio.dominio.entidades.Reserva;
 import com.nnm.nnm.persistencia.ReservaDAO;
-import com.nnm.nnm.persistencia.UsuarioDAO;
 
-
-
-@Controller
+@Service
 public class GestorReservas {
-    public static final Logger log = LoggerFactory.getLogger(GestorReservas.class);//Logger para poder hacer seguimiento de las acciones en el gestor de reservas
 
-   @Autowired
+    @Autowired
     private ReservaDAO reservaDAO;
 
-    @Autowired
-    private InmuebleDAO inmuebleDAO;
-
-    @Autowired
-    private UsuarioDAO usuarioDAO;
-
-    @GetMapping("/reservas")
-    public String reservasForm(Model model) {
-        model.addAttribute("reserva", new Reserva());
-        model.addAttribute("Propiedades", inmuebleDAO.findAll());
-        model.addAttribute("Usuarios", usuarioDAO.findAll());
-        log.info(reservaDAO.findAll().toString());
-        return "reserva";//html
+    // Verifica si una reserva existe en la base de datos por su ID
+    public boolean existeReserva(long id) {
+        return reservaDAO.findById(id) != null;
     }
 
-    @PostMapping("/reservas")
-    public String reservasSubmit(@ModelAttribute Reserva reserva, Model model) {
-        model.addAttribute("reserva", reserva);
-        Reserva savedReserva = reservaDAO.save(reserva);
-        log.info("Reserva guardada: " + savedReserva);
-        return "result";
+    // Registra una nueva reserva
+    public void registrarReserva(Reserva reserva) {
+        reservaDAO.save(reserva);
+    }
+
+    // Autentica si una reserva corresponde a un usuario específico
+    public boolean autenticarReserva(long idReserva, String usernameUsuario) {
+        Reserva reserva = reservaDAO.findById(idReserva);
+        return reserva != null && reserva.getUsername_inquilino().getLogin().equals(usernameUsuario);
+    }
+
+    // método para cancelar una reserva
+    public boolean cancelarReserva(long idReserva, String usernameUsuario) {
+        Reserva reserva = reservaDAO.findById(idReserva);
+        if(reserva != null && reserva.getUsername_inquilino().getLogin().equals(usernameUsuario)){
+            reservaDAO.delete(reserva);
+            return true;
+        }
+        return false;
     }
 
 }
-
-    
