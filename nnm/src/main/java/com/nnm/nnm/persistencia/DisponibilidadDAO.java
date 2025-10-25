dbeffb710ba1e0a96dff55e57e964e47b88e7d48
LauraRodriguez53 <Laura.Rodriguez@alu.uclm.es>
2025-10-25 17:23:26 +0200

    Creacion de la clase DisponibilidadDAO

---
+++ nnm/src/main/java/com/nnm/nnm/persistencia/DisponibilidadDAO.java
@@ -0,0 +1,13 @@
+package com.nnm.nnm.persistencia;
+
+import org.springframework.stereotype.Repository;
+
+import com.nnm.nnm.negocio.dominio.entidades.Disponibilidad;
+
+@Repository
+public class DisponibilidadDAO extends EntidadDAO<Disponibilidad, Long> {
+    
+    public DisponibilidadDAO() {
+        super(Disponibilidad.class);
+    }
+}
