6d78868aec8dfb53f5045ac26524f3b8057b6474
LauraRodriguez53 <Laura.Rodriguez@alu.uclm.es>
2025-10-26 15:55:56 +0100

    cambio y correccion de los gestores para mayor coherencia

--- nnm/src/main/java/com/nnm/nnm/negocio/controller/GestorDisponibilidad.java
+++ nnm/src/main/java/com/nnm/nnm/negocio/controller/GestorDisponibilidad.java
@@ -2,44 +2,22 @@ package com.nnm.nnm.negocio.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.nnm.nnm.negocio.dominio.entidades.Disponibilidad;
 import com.nnm.nnm.persistencia.DisponibilidadDAO;
+
 import java.util.List;
 
 @Service
 public class GestorDisponibilidad {
-   
+
     @Autowired
     private DisponibilidadDAO disponibilidadDAO;
 
-    // Consultar todas las disponibilidades
-    public List<Disponibilidad> listarDisponibilidades() {
-        return disponibilidadDAO.findAll();
+    public void registrarDisponibilidad(Disponibilidad d) {
+        disponibilidadDAO.save(d);
     }
 
-    // Consultar disponibilidad por Id
-    public Disponibilidad obtenerDisponibilidad(long id) {
-        return disponibilidadDAO.findById(id);
-    }
-
-    // Registrar o actualizar disponibilidad
-    public void guardarDisponibilidad(Disponibilidad disponibilidad) {
-        disponibilidadDAO.save(disponibilidad);
-    }
-
-    // Eliminar disponibilidad
-    public void eliminarDisponibilidad(long id) {
-        Disponibilidad d = disponibilidadDAO.findById(id);
-        if(d != null) {
-            disponibilidadDAO.delete(d);
-        }
-    }
-
-    // Consultar disponibilidades por inmueble
-    public List<Disponibilidad> listarPorInmueble(long idInmueble) {
-        return listarDisponibilidades().stream()
-                .filter(d -> d.getInmueble().getId() == idInmueble)
-                .toList();
+    public List<Disponibilidad> obtenerDisponibilidadPorInmueble(long id_inmueble) {
+        return disponibilidadDAO.findByInmueble(id_inmueble);
     }
 }
--- nnm/src/main/java/com/nnm/nnm/negocio/controller/GestorInmuebles.java
+++ nnm/src/main/java/com/nnm/nnm/negocio/controller/GestorInmuebles.java
@@ -6,25 +6,23 @@ import org.springframework.stereotype.Service;
 import com.nnm.nnm.negocio.dominio.entidades.Inmueble;
 import com.nnm.nnm.persistencia.InmuebleDAO;
 
+import java.util.List;
+
 @Service
-public class GestorInmuebles{
+public class GestorInmuebles {
 
     @Autowired
     private InmuebleDAO inmuebleDAO;
 
-    public boolean autenticarInmueble(long Id, String username_propietario ){
-        Inmueble inmueble = inmuebleDAO.findById(Id);
-        return (inmueble != null && inmueble.getUsername_propietario().equals(username_propietario));
-
-    }
-
-    //comprobamos que el Inmueble existe en nuestra base de datos
-    public boolean autenticarInmueble(long Id){
-        return inmuebleDAO.findById(Id) != null; //Si el inmueble ya existe devolvera true, si no nos dara False
-
-    }
-
-    public void registrarInmueble(Inmueble inmueble){
+    public void registrarInmueble(Inmueble inmueble) {
         inmuebleDAO.save(inmueble);
+    }
+
+    public List<Inmueble> listarInmuebles() {
+        return inmuebleDAO.findAll();
+    }
+
+    public Inmueble obtenerInmueblePorId(long id) {
+        return inmuebleDAO.findById(id);
     }
 }
