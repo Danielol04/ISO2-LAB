ac33455367cce8375a118c206069b30ff9771577
LauraRodriguez53 <Laura.Rodriguez@alu.uclm.es>
2025-10-26 15:58:53 +0100

    Correccion de las entidades DAO por incopatibilidad con la base de datos

--- nnm/src/main/java/com/nnm/nnm/persistencia/InmuebleDAO.java
+++ nnm/src/main/java/com/nnm/nnm/persistencia/InmuebleDAO.java
@@ -1,14 +1,26 @@
 package com.nnm.nnm.persistencia;
 
 import org.springframework.stereotype.Repository;
-
 import com.nnm.nnm.negocio.dominio.entidades.Inmueble;
+import java.util.List;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 
 @Repository
-public class InmuebleDAO extends EntidadDAO<Inmueble, Long> {
+public class InmuebleDAO {
 
-    public InmuebleDAO() {
-        super(Inmueble.class);
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(Inmueble i) {
+        em.persist(i);
     }
 
+    public List<Inmueble> findAll() {
+        return em.createQuery("FROM Inmueble", Inmueble.class).getResultList();
+    }
+
+    public Inmueble findById(long id) {
+        return em.find(Inmueble.class, id);
+    }
 }
