314201be0ee3a149855cc18352aa5981914e181c
Csauul <saulcorbelle@gmail.com>
2025-12-15 00:45:37 +0100

    Eliminacion panel detalles usuario

--- nnm/src/main/java/com/nnm/nnm/negocio/controller/GestorSolicitudes.java
+++ nnm/src/main/java/com/nnm/nnm/negocio/controller/GestorSolicitudes.java
@@ -4,8 +4,6 @@ import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,17 +11,12 @@ import com.nnm.nnm.negocio.dominio.entidades.EstadoReserva;
 import com.nnm.nnm.negocio.dominio.entidades.Inmueble;
 import com.nnm.nnm.negocio.dominio.entidades.Reserva;
 import com.nnm.nnm.negocio.dominio.entidades.SolicitudReserva;
-import com.nnm.nnm.persistencia.ReservaDAO;
 import com.nnm.nnm.persistencia.SolicitudReservaDAO;
-import com.nnm.nnm.presentacion.VentanaReserva;
 
 @Service
 public class GestorSolicitudes {
-    private static final Logger log = LoggerFactory.getLogger(VentanaReserva.class);
     @Autowired
     private SolicitudReservaDAO solicitudReservaDAO;
-    @Autowired
-    private ReservaDAO reservaDAO;
     @Autowired
     private GestorInmuebles gestorInmuebles;
     @Autowired
