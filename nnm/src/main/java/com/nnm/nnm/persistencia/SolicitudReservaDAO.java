a6041a9d3b810cffd55f3fb2337e9ea4e415868b
Daniel Salas <danielsalasconejo95@gmail.com>
2025-12-15 10:22:03 +0100

    Revert "Gestion de propiedades"

--- nnm/src/main/java/com/nnm/nnm/persistencia/SolicitudReservaDAO.java
+++
@@ -1,9 +0,0 @@
-package com.nnm.nnm.persistencia;
-
-import com.nnm.nnm.negocio.dominio.entidades.SolicitudReserva;
-
-public class SolicitudReservaDAO extends EntidadDAO<SolicitudReserva,Long> {
-    public SolicitudReservaDAO() {
-        super(SolicitudReserva.class);
-    }
-}
